13ae2bd86f712e62da16f4a9595dc4fb75d47211
tjfgk97 <tjfgk97@gmail.com>
2023-12-04 13:40:14 +0900

    first inteliJ

--- src/ch04_if/Ex08_IfExample.java
+++ src/ch04_if/Ex08_IfExample.java
@@ -31,16 +31,17 @@ public class Ex08_IfExample {
 
 		int max = 0;
 		if (num1 > num2) {
-			if(num1>num3) {
-			max = num1;
+			if (num1 > num3) {
+				max = num1;
 			}
 		} else {
-			if(num2>num3) {
-			max = num3;
-			}else {
+			if (num2 > num3) {
+				max = num3;
+			} else {
 				max = num3;
 			}
 		}
-System.out.println("가장 큰 수는 "+max+"입니다.");
+		System.out.println("가장 큰 수는 " + max + "입니다.");
+		System.out.println("추가된 부분입니다.");
 	}
 }
