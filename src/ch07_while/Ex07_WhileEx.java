04a668163e9b4498e55b9414e81d1fc3079ce7cb
tjfgk97 <tjfgk97@gmail.com>
2023-12-06 17:58:57 +0900

    무한반복 while문-boolean 변수 사용 ver

--- src/ch07_while/Ex07_WhileEx.java
+++ src/ch07_while/Ex07_WhileEx.java
@@ -3,26 +3,28 @@ package ch07_while;
 public class Ex07_WhileEx {
     public static void main(String[] args) {
         /*
-        * 1 + (-2) + 3 + (-4) + 5 + (-6)처럼
-        * 계산을 했을 때 합계가 100이 됐을 때의 숫자를 출력
-        * 정답 : 199*/
+         * 1 + (-2) + 3 + (-4) + 5 + (-6)처럼
+         * 계산을 했을 때 합계가 100이 됐을 때의 숫자를 출력
+         * 정답 : 199*/
 
+
+//      break가 반드시 필요한 걸 같이 써야하는 경우
+//      break가 겹쳐서 실행이 되지 않을 수 있기 때문에
+//      무한반복 while문을 쓸 때는 boolean 타입 변수 사용하는 것에 익숙해질 것!
         int i = 0;
         int sum = 0;
-        while(true){
-
-            if(sum != 100){
+        boolean run = true;
+        while (run) {
+            if (sum != 100) {
                 i++;
-                if(i % 2 == 0){
+                if (i % 2 == 0) {
                     sum = sum - i;
-                }else{
+                } else {
                     sum = sum + i;
                 }
-
-            }else{
-                break;
+            } else {
+                run = false;
             }
-
         }
         System.out.println(i);
     }
