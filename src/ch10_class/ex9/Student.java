5dfb8c9329a40041e3191af93b31164aa212a23f
tjfgk97 <tjfgk97@gmail.com>
2023-12-13 09:18:56 +0900

    객체지향-클래스 활용

--- src/ch10_class/ex9/Student.java
+++ src/ch10_class/ex9/Student.java
@@ -1,6 +1,9 @@
 package ch10_class.ex9;
 
 public class Student {
+    // 필드, getter/setter, 기본생성자, 매개변수 생성자, toString
+    // ↑↑(이 다섯가지가 한 세트로 필수이다!)↑↑
+
     /*
         필드
         이름(studentName)
@@ -15,40 +18,46 @@ public class Student {
 
 
     // 각 필드의 setter 메서드
-    public void setStudentName(String studentName){
+    public void setStudentName(String studentName) {
         this.studentName = studentName;
     }
-    public void setStudentNumber(String studentNumber){
+
+    public void setStudentNumber(String studentNumber) {
         this.studentNumber = studentNumber;
     }
-    public void setStudentMajor(String studentMajor){
+
+    public void setStudentMajor(String studentMajor) {
         this.studentMajor = studentMajor;
     }
-    public void setStudentMobile(String studentMobile){
+
+    public void setStudentMobile(String studentMobile) {
         this.studentMobile = studentMobile;
     }
 
     // 각 필드의 getter 메서드
-    public String getStudentName(){
+    public String getStudentName() {
         return studentName;
     }
-    public String getStudentNumber(){
+
+    public String getStudentNumber() {
         return studentNumber;
     }
-    public String getStudentMajor(){
+
+    public String getStudentMajor() {
         return studentMajor;
     }
-    public String getStudentMobile(){
+
+    public String getStudentMobile() {
         return studentMobile;
     }
 
     // 기본 생성자
-    public Student(){
+    public Student() {
         System.out.println("기본 생성자");
     }
 
     // 모든 필드를 매개변수로 하는 생성자
-    public Student(String studentName, String studentNumber, String studentMajor, String studentMobile){
+    public Student(String studentName, String studentNumber, String studentMajor, String studentMobile) {
         this.studentName = studentName;
         this.studentNumber = studentNumber;
         this.studentMajor = studentMajor;
@@ -58,10 +67,25 @@ public class Student {
     }
 
     // 필드에 저장된 값을 출력하는 print() 메서드
-    public void print(){
+    public void print() {
         System.out.println(getStudentName());
         System.out.println(getStudentNumber());
         System.out.println(getStudentMajor());
         System.out.println(getStudentMobile());
+    }
+
+    /**
+     * method name : toString
+     * return type : String
+     * parameter : x
+     */
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentName = " + studentName + ", " +
+                "studentNumber = " + studentNumber + ", " +
+                "studentMajor = " + studentMajor + "," +
+                " studentMobile = " + studentMobile +"}";
     }
 }
--- src/ch10_class/ex9/StudentMain.java
+++ src/ch10_class/ex9/StudentMain.java
@@ -3,8 +3,10 @@ package ch10_class.ex9;
 public class StudentMain {
     public static void main(String[] args) {
 
-        // 기본생성자로 Student 클래스 객체 만들고 setter를 이용하여 필드값 채우기
+        // 기본생성자로 Student 클래스 객체 만들고
         Student student = new Student();
+
+        // setter를 이용하여 필드값 채우기
         student.setStudentName("학생1");
         student.setStudentNumber("231212");
         student.setStudentMajor("컴퓨터공학");
@@ -20,7 +22,8 @@ public class StudentMain {
 
         // 각각의 객체에서 print 메서드 호출하여 필드값 출력
 
-        student.print();
+        student.print(); // setter를 이용해서 채운 필드값이 print 메서드로 대입돼서 출력됨
+        student1.print();// 매개변수로 넘겨준 값이 print 메서드로 대입되어 출력됨
 
 
     }
